7a652a27dbc0a4d4c710c1247ab8c1bbe17d615c
Julian Poyourow <julianpoyo@gmail.com>
2015-03-22 15:47:12 -0700

    requestRSVP function

--- events.php
+++ events.php
@@ -395,6 +395,81 @@ function deleteEvent($id) {
 }
 
 function requestRSVP(){
+    $request = Slim::getInstance()->request();
+    $body = $request->getBody();
+    $requestjson = json_decode($body);
+
+    $sql = "SELECT
+
+        user_id
+
+        FROM sessions WHERE token=:token LIMIT 1";
+
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("token", $requestjson->session_token);
+        $stmt->execute();
+        $session = $stmt->fetchObject();
+        $db = null;
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+
+    if(!isset($session->user_id)){
+        echo '{"error":{"text":"Token is not valid","errorid":"12"}}';
+        exit;
+    }
+
+    $sql = "SELECT status FROM event_attendees 
+
+    WHERE attendee_id=:myuserid AND event_id=:event_id
+
+    ";
+
+    try {
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam("myuserid", $session->user_id);
+        $stmt->bindParam("event_id", $requestjson->event_id);
+        
+        $stmt->execute();
+        $db = null;
+        $rsvp_status = $stmt->fetchObject();
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+
+
+    if($rsvp_status == false){
+        $rsvp_status = 1;
+
+        $sql = "INSERT INTO event_attendees 
+    
+        (event_id, attendee_id, status)
+        VALUES
+        (:event_id, :myuserid, :status)
+
+        ";
+
+        try {
+            $db = getConnection();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindParam("event_id", $requestjson->event_id);
+            $stmt->bindParam("myuserid", $session->user_id);
+            $stmt->bindParam("status", $rsvp_status);
+            
+            $stmt->execute();
+            $db = null;
+            echo json_encode($requestjson);
+        } catch(PDOException $e) {
+            echo '{"error":{"text":'. $e->getMessage() .'}}';
+        }
+    } else {
+        echo '{"error":{"text":"Event already added","errorid":"233"}}';
+    }
 }
 
 function inviteRSVP(){
