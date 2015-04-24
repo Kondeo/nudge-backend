b6169034da6319d16f085dd9a25b927d6059dd1f
Julian Poyourow <julianpoyo@gmail.com>
2015-04-24 12:45:55 -0700

    Added cancelRSVP functionality

--- events.php
+++ events.php
@@ -822,6 +822,53 @@ function acceptRSVPInvite(){
 
 //Cancel an rsvp
 function cancelRSVP(){
+  $request = Slim::getInstance()->request();
+  $body = $request->getBody();
+  $requestjson = json_decode($body);
+
+  $sql = "SELECT
+
+      user_id
+
+      FROM sessions WHERE token=:token LIMIT 1";
+
+  try {
+      $db = getConnection();
+      $stmt = $db->prepare($sql);
+      $stmt->bindParam("token", $requestjson->session_token);
+      $stmt->execute();
+      $session = $stmt->fetchObject();
+      $db = null;
+  } catch(PDOException $e) {
+      echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+
+  if(!isset($session->user_id)){
+      echo '{"error":{"text":"Token is not valid","errorid":"12"}}';
+      exit;
+  }
+
+  $sql = "DELETE FROM event_attendees
+    WHERE attendee_id=:attendee_id AND event_id=:event_id
+  ";
+
+  if($requestjson->attendee_id == "me"){
+    $requestjson->attendee_id = $session->user_id;
+  }
+
+  try {
+      $db = getConnection();
+      $stmt = $db->prepare($sql);
+
+      $stmt->bindParam("attendee_id", $requestjson->attendee_id);
+      $stmt->bindParam("event_id", $requestjson->event_id);
+
+      $stmt->execute();
+      $db = null;
+      $rsvp_status = $stmt->fetchObject();
+  } catch(PDOException $e) {
+      echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
 }
 
 //Get all current pending rsvps
